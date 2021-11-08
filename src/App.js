f22740b012ebc09cf2bc9d220473f27e5a30f78b
coraline chau <coraline.chau@gmail.com>
2021-11-08 16:04:54 +0100

    Ajout d'un bt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
@@ -7,10 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="50" />
         <h2>header</h2>
-        
+
       </header>
       <section>
         <h1>Contenu</h1>
+        <button type="button" class="btn btn-primary">Primary</button>
       </section>
       <footer>
         <h2>footer</h2>
