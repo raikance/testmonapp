b308d59f8f2ecf6a5962147db58d8886a3682543
coraline chau <coraline.chau@gmail.com>
2021-11-08 16:46:43 +0100

    Footer ok

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="50" />
+
+    
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo" width="50" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Gallery</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      
         <h2>header</h2>
 
       </header>
@@ -15,9 +42,7 @@ function App() {
         <h1>Contenu</h1>
         <button type="button" class="btn btn-primary">Primary</button>
       </section>
-      <footer>
-        <h2>footer</h2>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+    const Footer = () => {
+    return(
+      <footer className="bg-dark text-white text-center p-2"> 
+        <h2>Ola quetal</h2>
+      </footer>
+      )
+    }
+    export default Footer
